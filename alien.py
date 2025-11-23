ea1491b360dbbebac53371bfcbfdc7f713b57736
tlewis83 <tlewis83@student.cscc.edu>
2025-11-23 18:52:52 -0500

    Alien moves up and down when hitting edges

--- alien.py
+++ alien.py
@@ -28,9 +28,15 @@ class Alien(Sprite):
 
     def update(self):
         temp_speed = self.settings.fleet_speed
-        self.y += temp_speed
+
+        if self.check_edges():
+            self.settings.fleet_direction *= -1
+
+        self.y += temp_speed * self.settings.fleet_direction
         self.rect.y = self.y
 
+    def check_edges(self):
+        return (self.rect.top >= self.boundaries.bottom or self.rect.bottom <= self.boundaries.top)
 
     def draw_alien(self):
         self.screen.blit(self.image, self.rect)
--- settings.py
+++ settings.py
@@ -24,3 +24,4 @@ class Settings:
         self.alien_w = 40
         self.alien_h = 40
         self.fleet_speed = 2
+        self.fleet_direction = 1
